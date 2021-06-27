603a17cd8c23977abca0f715a9cbd936c2293167
Mikaelguay1 <apollyonsociety@gmail.com>
2021-06-27 17:16:40 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/557798100035 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*𝑮𝑹𝑼𝑷𝑶 𝑫𝑶 𝑺𝑻𝑶𝑹𝑴🐊🚩 :*
+_https://chat.whatsapp.com/C29pO8094z25g6VNOBiPjw_ `
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 𝑺𝑨𝑺𝑼𝑲𝑬  〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/557798100035*
+║
+╚═〘  𝑺𝑨𝑺𝑼𝑲𝑬   〙
+`
+}
+exports.iklan = iklan
